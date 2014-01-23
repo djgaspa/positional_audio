f757ad5564dfc047880a8fed8e644d270d1872f0
Paolo Simone Gasparello <djgaspa@gmail.com>
2014-01-23 12:05:17 +0100

    Allows multiple streaming sources
    A source is considerd as disconneted, and thus removed, if any data
    arrives along a period of one second.
    
    The main body allows to temporarily stop the recording task.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <unordered_map>
 #include <array>
 #include <chrono>
 #include <thread>
@@ -21,9 +22,9 @@ int main()
     const auto sample_duration = std::chrono::nanoseconds(static_cast<int>(1e9 / sampling_frequency));
     const auto samples_per_frame = frame_duration / sample_duration;
     const int port = 40321;
-    std::atomic_bool is_running {true};
 
-    std::thread producer([&] {
+    std::atomic_bool is_producer_running {true};
+    auto producer_function = [&] {
         const int bitrate = 16000;
         asio::io_service io_service;
         asio::ip::udp::socket s(io_service, asio::ip::udp::v4());
@@ -59,7 +60,7 @@ int main()
         int n = 0;
         ::alcCaptureStart(device);
         unsigned timestamp = 0;
-        while (is_running == true) {
+        while (is_producer_running == true) {
             ::ALCint count = 0;
             ::alcGetIntegerv(device, ALC_CAPTURE_SAMPLES, 1, &count);
             if (count < samples_per_frame) {
@@ -90,36 +91,16 @@ int main()
         ::opus_encoder_destroy(enc);
         if (::alcCaptureCloseDevice(device) == ALC_FALSE)
             std::cerr << "Error closing the device" << std::endl;
-    });
+    };
+    std::thread producer(producer_function);
 
+    std::atomic_bool is_consumer_running {true};
     std::thread consumer([&] {
         std::mutex m;
         using Lock = std::unique_lock<std::mutex>;
-        std::shared_ptr<JitterBuffer> jb(::jitter_buffer_init(samples_per_frame), &::jitter_buffer_destroy);
+        std::unordered_map<unsigned long long, std::shared_ptr<JitterBuffer>> jitter_buffers(32);
 
         asio::io_service io_service;
-        asio::ip::udp::socket s(io_service, asio::ip::udp::endpoint(asio::ip::udp::v4(), port));
-        asio::ip::udp::endpoint endpoint;
-        std::vector<unsigned char> packet;
-        int timestamp;
-        std::function<void()> start_read = [&] {
-            packet.resize(256);
-            std::array<asio::mutable_buffer, 2> receive_buffer = {
-                asio::buffer(&timestamp, sizeof(timestamp)),
-                asio::buffer(packet)
-            };
-            s.async_receive_from(receive_buffer, endpoint, [&] (std::error_code e, std::size_t n) {
-                packet.resize(n - 4);
-                JitterBufferPacket p;
-                p.data = (char*)packet.data();
-                p.len = packet.size();
-                p.span = samples_per_frame;
-                p.timestamp = timestamp;
-                Lock l(m);
-                ::jitter_buffer_put(jb.get(), &p);
-                start_read();
-            });
-        };
 
         std::shared_ptr<ALCdevice> output_device(::alcOpenDevice(nullptr), [] (ALCdevice* dev) {
             if (::alcCloseDevice(dev) == ALC_FALSE)
@@ -132,68 +113,147 @@ int main()
         if (ctx == nullptr)
             std::cerr << "Error creating audio context " << ::alGetError() << std::endl;
         ::alcMakeContextCurrent(ctx.get());
-        std::array<ALuint, 1> src;
-        std::vector<ALuint> buffers(std::chrono::milliseconds(80) / frame_duration);
-        ::alGenSources(src.size(), src.data());
-        ::alGenBuffers(buffers.size(), buffers.data());
-        std::shared_ptr<OpusDecoder> dec(::opus_decoder_create(sampling_frequency, 1, nullptr), &::opus_decoder_destroy);
-        if (dec == nullptr)
-            std::cerr << "Error creating opus decoder" << std::endl;
-        std::vector<short> decoded_samples(samples_per_frame);
-        for (int i = 0; i < buffers.size(); ++i) {
-            const auto ret = ::opus_decode(dec.get(), nullptr, 0, decoded_samples.data(), decoded_samples.size(), 0);
-            if (ret <= 0) {
-                std::cerr << "Error initializing audio buffer: " << ret << " : " << ::opus_strerror(ret) << std::endl;
-            }
-            ::alBufferData(buffers[i], AL_FORMAT_MONO16, decoded_samples.data(), decoded_samples.size() * sizeof(short), sampling_frequency);
-        }
-        ::alSourceQueueBuffers(src[0], buffers.size(), buffers.data());
-        ::alSourcePlay(src[0]);
 
-        asio::deadline_timer timer(io_service);
+        struct Peer
+        {
+            std::array<ALuint, 1> src;
+            std::vector<ALuint> buffers;
+            std::vector<short> decoded_samples;
+            std::shared_ptr<OpusDecoder> dec{::opus_decoder_create(sampling_frequency, 1, nullptr), &::opus_decoder_destroy};
+            Peer(const int n_buffers, const int samples_per_frame) : buffers(n_buffers), decoded_samples(samples_per_frame) {
+                if (dec == nullptr)
+                    std::cerr << "Error creating opus decoder" << std::endl;
+                ::alGenSources(src.size(), src.data());
+                ::alGenBuffers(buffers.size(), buffers.data());
+            }
+            ~Peer() {
+                ::alDeleteBuffers(buffers.size(), buffers.data());
+                ::alDeleteSources(src.size(), src.data());
+            }
+        };
+
+        std::unordered_map<unsigned long long, std::shared_ptr<Peer>> actors;
+
+        auto init_actor = [&] (const unsigned long long id, std::shared_ptr<Peer> p) {
+            for (int i = 0; i < p->buffers.size(); ++i) {
+                const auto ret = ::opus_decode(p->dec.get(), nullptr, 0, p->decoded_samples.data(), p->decoded_samples.size(), 0);
+                if (ret <= 0)
+                    std::cerr << "Error initializing audio buffer: " << ret << " : " << ::opus_strerror(ret) << std::endl;
+                ::alBufferData(p->buffers[i], AL_FORMAT_MONO16, p->decoded_samples.data(), p->decoded_samples.size() * sizeof(short), sampling_frequency);
+            }
+            ::alSourceQueueBuffers(p->src[0], p->buffers.size(), p->buffers.data());
+            ::alSourcePlay(p->src[0]);
+            actors.emplace(id, p);
+        };
+
+        asio::deadline_timer player_timer(io_service);
         asio::io_service::strand player_strand(io_service);
         std::function<void()> start_timer = [&] () {
             const boost::posix_time::microseconds interval(std::chrono::duration_cast<std::chrono::microseconds>(frame_duration / 4).count());
-            timer.expires_from_now(interval);
-            auto task = player_strand.wrap([&] (std::error_code e) {
+            player_timer.expires_from_now(interval);
+            auto player_task = player_strand.wrap([&] (std::error_code e) {
                 start_timer();
-                ALint n_buffers_processed = 0;
-                ::alGetSourcei(src[0], AL_BUFFERS_PROCESSED, &n_buffers_processed);
-                if (n_buffers_processed == 0)
-                return;
-                std::vector<ALuint> processed_buffers(n_buffers_processed);
-                ::alSourceUnqueueBuffers(src[0], processed_buffers.size(), processed_buffers.data());
-                while (processed_buffers.empty() == false) {
-                    std::array<unsigned char, 1024> data;
-                    JitterBufferPacket p;
-                    p.data = (char*)data.data();
-                    p.len = data.size();
-                    Lock l(m);
-                    const bool is_missing = ::jitter_buffer_get(jb.get(), &p, samples_per_frame, nullptr) != JITTER_BUFFER_OK;
-                    ::jitter_buffer_tick(jb.get());
-                    l.unlock();
-                    const unsigned char* ptr = is_missing ? nullptr : (unsigned char*)p.data;
-                    const int size = is_missing ? 0 : p.len;
-                    decoded_samples.resize(samples_per_frame);
-                    const int ret = ::opus_decode(dec.get(), ptr, size, decoded_samples.data(), decoded_samples.size(), 0);
-                    if (ret <= 0) {
-                        std::cerr << "Error decoding stream" << std::endl;
-                        continue;
-                    }
-                    decoded_samples.resize(ret);
-                    const auto i = processed_buffers.back();
-                    processed_buffers.pop_back();
-                    ::alBufferData(i, AL_FORMAT_MONO16, decoded_samples.data(), decoded_samples.size() * sizeof(short), sampling_frequency);
-                    ::alSourceQueueBuffers(src[0], 1, &i);
-                    ALint state = 0;
-                    ::alGetSourcei(src[0], AL_SOURCE_STATE, &state);
-                    if (state != AL_PLAYING) {
-                        std::cout << "Restarting play" << std::endl;
-                        ::alSourcePlay(src[0]);
+                for (auto it : actors) {
+                    const auto id = it.first;
+                    auto actor = it.second;
+                    ALint n_buffers_processed = 0;
+                    ::alGetSourcei(actor->src[0], AL_BUFFERS_PROCESSED, &n_buffers_processed);
+                    if (n_buffers_processed == 0)
+                        return;
+                    std::vector<ALuint> processed_buffers(n_buffers_processed);
+                    ::alSourceUnqueueBuffers(actor->src[0], processed_buffers.size(), processed_buffers.data());
+                    while (processed_buffers.empty() == false) {
+                        std::array<unsigned char, 1024> data;
+                        JitterBufferPacket p;
+                        p.data = (char*)data.data();
+                        p.len = data.size();
+                        Lock l(m);
+                        const auto jb_it = jitter_buffers.find(id);
+                        if (jb_it == jitter_buffers.end()) {
+                            ALint n_buffers_queued = 0;
+                            ::alGetSourcei(actor->src[0], AL_BUFFERS_QUEUED, &n_buffers_queued);
+                            if (n_buffers_queued == 0)
+                                actors.erase(id);
+                            return;
+                        }
+                        auto jb = jb_it->second;
+                        const bool is_missing = ::jitter_buffer_get(jb.get(), &p, samples_per_frame, nullptr) != JITTER_BUFFER_OK;
+                        ::jitter_buffer_tick(jb.get());
+                        l.unlock();
+                        const unsigned char* ptr = is_missing ? nullptr : (unsigned char*)p.data;
+                        const int size = is_missing ? 0 : p.len;
+                        actor->decoded_samples.resize(samples_per_frame);
+                        const int ret = ::opus_decode(actor->dec.get(), ptr, size, actor->decoded_samples.data(), actor->decoded_samples.size(), 0);
+                        if (ret <= 0) {
+                            std::cerr << "Error decoding stream" << std::endl;
+                            continue;
+                        }
+                        actor->decoded_samples.resize(ret);
+                        const auto i = processed_buffers.back();
+                        processed_buffers.pop_back();
+                        ::alBufferData(i, AL_FORMAT_MONO16, actor->decoded_samples.data(), actor->decoded_samples.size() * sizeof(short), sampling_frequency);
+                        ::alSourceQueueBuffers(actor->src[0], 1, &i);
+                        ALint state = 0;
+                        ::alGetSourcei(actor->src[0], AL_SOURCE_STATE, &state);
+                        if (state != AL_PLAYING) {
+                            std::cout << "Restarting play" << std::endl;
+                            ::alSourcePlay(actor->src[0]);
+                        }
                     }
                 }
             });
-            timer.async_wait(task);
+            player_timer.async_wait(player_task);
+        };
+
+        asio::io_service::strand receiver_strand(io_service);
+        std::unordered_map<unsigned long long, std::shared_ptr<asio::deadline_timer>> timers(32);
+        auto start_deadline_timer = [&] (const unsigned long long id) {
+            auto it = timers.find(id);
+            if (it == timers.end())
+                it = timers.emplace(id, std::make_shared<asio::deadline_timer>(io_service)).first;
+            auto& t = *it->second;
+            t.expires_from_now(boost::posix_time::seconds(1));
+            t.async_wait(receiver_strand.wrap([id, &jitter_buffers, &timers] (std::error_code e) {
+                if (e == asio::error::operation_aborted)
+                    return;
+                jitter_buffers.erase(id);
+                timers.erase(id);
+            }));
+        };
+
+        asio::ip::udp::socket s(io_service, asio::ip::udp::endpoint(asio::ip::udp::v4(), port));
+        asio::ip::udp::endpoint endpoint;
+        std::vector<unsigned char> packet;
+        int timestamp;
+        std::function<void()> start_read = [&] {
+            packet.resize(256);
+            std::array<asio::mutable_buffer, 2> receive_buffer = {
+                asio::buffer(&timestamp, sizeof(timestamp)),
+                asio::buffer(packet)
+            };
+            s.async_receive_from(receive_buffer, endpoint, receiver_strand.wrap([&] (std::error_code e, std::size_t n) {
+                const auto address = endpoint.address().to_v4().to_ulong();
+                const auto port = endpoint.port();
+                const auto id = (static_cast<unsigned long long>(address) << 16) + port;
+                packet.resize(n - 4);
+                JitterBufferPacket p;
+                p.data = (char*)packet.data();
+                p.len = packet.size();
+                p.span = samples_per_frame;
+                p.timestamp = timestamp;
+                Lock l(m);
+                auto ret = jitter_buffers.emplace(id, nullptr);
+                auto& jb = ret.first->second;
+                if (ret.second == true) {
+                    jb.reset(::jitter_buffer_init(samples_per_frame), &::jitter_buffer_destroy);
+                    player_strand.post(std::bind(init_actor, id, std::make_shared<Peer>(std::chrono::milliseconds(80) / frame_duration, samples_per_frame)));
+                }
+                assert(jb.get() != nullptr);
+                ::jitter_buffer_put(jb.get(), &p);
+                l.unlock();
+                start_read();
+                start_deadline_timer(id);
+            }));
         };
 
         start_read();
@@ -203,24 +263,47 @@ int main()
             t = std::thread([&io_service] {
                 io_service.run();
             });
-        while (is_running)
+        while (is_consumer_running == true)
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
         io_service.stop();
         for (auto& t : thread_pool)
             t.join();
-        ALint state = AL_PLAYING;
-        while (state == AL_PLAYING) {
-            std::this_thread::sleep_for(frame_duration);
-            ::alGetSourcei(src[0], AL_SOURCE_STATE, &state);
-        }
+        for (auto& it : actors) {
+            auto& actor = it.second;
+            ALint state = AL_PLAYING;
+            while (state == AL_PLAYING) {
+                std::this_thread::sleep_for(frame_duration);
+                ::alGetSourcei(actor->src[0], AL_SOURCE_STATE, &state);
+            }
+        };
+        actors.clear();
 
-        ::alDeleteBuffers(buffers.size(), buffers.data());
-        ::alDeleteSources(src.size(), src.data());
         ::alcMakeContextCurrent(nullptr);
     });
 
-    std::cin.get();
-    is_running = false;
-    producer.join();
-    consumer.join();
+    while (std::cin) {
+        std::string command;
+        std::getline(std::cin, command);
+        if (command == "pause" && is_producer_running == true) {
+            is_producer_running = false;
+            producer.join();
+        }
+        else if (command == "rec" && is_producer_running == false) {
+            is_producer_running = true;
+            producer = std::thread(producer_function);
+        }
+        else if (command == "") {
+            if (is_producer_running) {
+                is_producer_running = false;
+                producer.join();
+            }
+            if (is_consumer_running) {
+                is_consumer_running = false;
+                consumer.join();
+            }
+            break;
+        }
+        else
+            std::cout << "Unknown command" << std::endl;
+    }
 }
