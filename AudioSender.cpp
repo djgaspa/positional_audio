489a1c022ea40fbe8fecaeb95b3dd831c73a0af4
Paolo Simone Gasparello <djgaspa@gmail.com>
2014-02-04 17:06:31 +0100

    AudioSender works asynchronously

--- AudioSender.cpp
+++ AudioSender.cpp
@@ -18,9 +18,6 @@ void AudioSender::run(const std::string &address, const short port, const int sa
     const auto frame_duration = std::chrono::milliseconds(frame_duration_ms);
     const auto sample_duration = std::chrono::nanoseconds(static_cast<int>(1e9 / sampling_frequency));
     const auto samples_per_frame = frame_duration / sample_duration;
-    asio::io_service io_service;
-    asio::ip::udp::socket s(io_service, asio::ip::udp::v4());
-    s.connect(asio::ip::udp::endpoint(asio::ip::address::from_string(address), port));
     auto* device = ::alcCaptureOpenDevice(nullptr, sampling_frequency, AL_FORMAT_MONO16, 1000 * samples_per_frame);
     if (device == nullptr) {
         std::cerr << "Error opening input device" << std::endl;
@@ -28,7 +25,6 @@ void AudioSender::run(const std::string &address, const short port, const int sa
     }
     //std::cout << "Input device: " << ::alcGetString(device, ALC_CAPTURE_DEVICE_SPECIFIER) << std::endl;
     using clock = std::chrono::steady_clock;
-    const auto t0 = clock::now();
     std::vector<short> samples(samples_per_frame);
     std::vector<unsigned char> buffer;
     auto* enc = ::opus_encoder_create(sampling_frequency, 1, OPUS_APPLICATION_VOIP, nullptr);
@@ -49,39 +45,69 @@ void AudioSender::run(const std::string &address, const short port, const int sa
     std::cout << "Complexity: " << complexity << std::endl;
     std::cout << "Max Bandwidth: " << max_bw << std::endl;
     std::cout << "FEC: " << (fec == 0 ? "NO" : "YES") << std::endl;
-    int n = 0;
-    ::alcCaptureStart(device);
+
+    asio::io_service io_service;
+    asio::ip::udp::socket s(io_service, asio::ip::udp::v4());
+    asio::steady_timer timer(io_service, clock::now());
+
+    const auto t0 = timer.expires_at();
+    int byte_counter = 0;
     unsigned timestamp = 0;
-    auto deadline = clock::now();
-    while (is_running == true) {
-        deadline += frame_duration;
-        std::this_thread::sleep_until(deadline);
-        ::ALCint count = 0;
-        ::alcGetIntegerv(device, ALC_CAPTURE_SAMPLES, sizeof(count), &count);
-        if (count < samples_per_frame) {
-            continue;
-        }
-        ::alcCaptureSamples(device, samples.data(), samples_per_frame);
-        buffer.resize(256);
-        const int ret = ::opus_encode(enc, samples.data(), samples_per_frame, buffer.data(), buffer.size());
-        if (ret < 0)
-            std::cerr << "Error encoding" << std::endl;
-        if (ret > 1) {
-            n += ret;
-            buffer.resize(ret);
-            std::array<asio::const_buffer, 2> send_buffer {
-                asio::buffer(&timestamp, sizeof(timestamp)),
-                        asio::buffer(buffer)
-            };
-            s.send(send_buffer);
-        }
-        timestamp += samples_per_frame;
-    }
+
+
+    std::array<asio::const_buffer, 2> send_buffer {
+        asio::buffer(&timestamp, sizeof(timestamp)),
+                asio::buffer(buffer)
+    };
+
+    std::function<void()> start_record = [&] {
+        timer.expires_at(timer.expires_at() + frame_duration);
+        timer.async_wait([&] (const std::error_code& e) {
+            if (e)
+                return;
+            start_record();
+            ::ALCint count = 0;
+            ::alcGetIntegerv(device, ALC_CAPTURE_SAMPLES, sizeof(count), &count);
+            if (count < samples_per_frame)
+                return;
+            ::alcCaptureSamples(device, samples.data(), samples_per_frame);
+            buffer.resize(256);
+            const int ret = ::opus_encode(enc, samples.data(), samples_per_frame, buffer.data(), buffer.size());
+            if (ret < 0)
+                std::cerr << "Error encoding" << std::endl;
+            if (ret > 1) {
+                buffer.resize(ret);
+                send_buffer[1] = asio::buffer(buffer);
+                s.async_send(send_buffer, [&byte_counter] (const std::error_code& e, std::size_t n) {
+                    if (e)
+                        return;
+                    byte_counter += n;
+                });
+            }
+            timestamp += samples_per_frame;
+        });
+    };
+
+    s.async_connect(asio::ip::udp::endpoint(asio::ip::address::from_string(address), port), [&] (const std::error_code& e) {
+        if (e)
+            return;
+        ::alcCaptureStart(device);
+        start_record();
+    });
+
+    std::thread thread([&] {
+        io_service.run();
+    });
+
+    while (is_running == true)
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    io_service.stop();
+    thread.join();
     ::alcCaptureStop(device);
     const auto t1 = clock::now();
     const double t = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count() / 1000.0;
     std::cout << "Time: " << t << std::endl;
-    std::cout << "Bandwidth: " << n * 8 / t << " bps" << std::endl;
+    std::cout << "Bandwidth: " << byte_counter * 8 / t << " bps" << std::endl;
     ::opus_encoder_destroy(enc);
     if (::alcCaptureCloseDevice(device) == ALC_FALSE)
         std::cerr << "Error closing the device" << std::endl;
